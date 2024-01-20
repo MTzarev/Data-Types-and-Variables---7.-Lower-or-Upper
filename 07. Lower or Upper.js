75cc4a7bf118ae6c0197f6cd748a15f13715fcee
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-20 09:30:28 +0200

    Add files via upload

---
+++ 07. Lower or Upper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(char) {
+    if (char === char.toUpperCase()) {
+        console.log("upper-case");
+    } else {
+        console.log("lower-case");
+    }
+
+
+    console.log();
+}
+lowerOrUpper("L");
+lowerOrUpper("f");
